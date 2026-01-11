5d85e22c59079ff05f09c0d08d9427e8f4347f14
xlsxmp <xlsxmp@gmail.com>
2026-01-11 22:39:21 +0800

    Fix syntax error in JSON structure

--- XHTTP-cf-CA.sh
+++ XHTTP-cf-CA.sh
@@ -124,7 +124,7 @@ cat > /etc/xray/config.json <<EOF
           ]
         },
         "xhttpSettings": {
-          "path": "$PATH_ID"，
+          "path": "$PATH_ID",
           "mode": "packet-up"
         }
       }
